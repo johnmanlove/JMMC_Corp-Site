67415a953d297aee273bbf3cef688d48a3907bb2
Rick Celeita <rick@johnmanlove.com>
2015-11-11 16:40:18 -0600

    Ready for home
    Commit for work from home

---
+++ wp-content/script/js.js
@@ -0,0 +1,13 @@
+jQuery(function(){
+	// MOVE TITLE
+	var $title 		= jQuery('.insideframe .title');
+	var $article 	= jQuery('.bottom .date');
+	var $i 			= 0;
+
+	while($i <= 4) {
+		jQuery($title[$i]).prependTo($article[$i]);
+		$i++;
+		//alert($i);
+	}
+
+});
