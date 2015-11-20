043ad5bdd6248ea5b54fd466adaedcc0f6eb514e
Rick Celeita <rceleita@gmail.com>
2015-11-19 18:29:25 -0600

    Commit from working from home
    Commit 11/19

--- wp-content/themes/responsive-child/internal-page.php
+++ wp-content/themes/responsive-child/internal-page.php
@@ -69,8 +69,36 @@ get_header(); ?>
 
 <div class="su-row video-gallery">
 	<div class="su-column su-column-size-1-1">
+		<h2>Media Library</h2>
 		<?php echo do_shortcode( '[rev_slider alias="vm-slider"]' ); ?>
 	</div>
 </div>
+<div class="su-row spheres-row">
+	<div class="su-column su-column-size-1-1">
+		<img class="size-full wp-image-83 aligncenter" src="http://localhost:8888/JMMC_Corp-Site/wp-content/uploads/2015/11/logo_white.png" alt="logo_white" width="367" height="174" />
+	</div>
+</div>
+<div class="su-row contact">
+	<div class="su-column su-column-size-2-5 text">
+		<h3>Receive the latest news from JMMC</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		<span class="legal"><p>Legal terms</p>
+			<p>You are entitled to access, correct and delete personal data which relates to you, and to object to the processing of your personal data, subject to legitimate grounds. To exercise these rights, please write to the following address [Protection des données personnelles JMMC – Communications Director – 5125 Preston Ave. | Houston, Texas 77505 or contact us via our contact form.</p></span>
+	</div>
+	<div class="su-column su-column-size-3-5 contact-form">
+		<h3>Please fill in your details below:</h3>
+		<p>All fields are required</p>
+		<?php echo do_shortcode( '[constantcontactapi formid="1"]' ); ?>
+	</div>
+</div>
+<?php echo do_shortcode('[su_row class="lets-talk"]
+
+[su_column size="1/2" class="spheres"]<img src="/JMMC_Corp-Site/wp-content/uploads/2015/11/spheres.png" alt="JMMC Spheres of Influence" />[/su_column]
+
+[su_column size="1/2"]
+<h3 style="text-align: center;">Now you know about us,</h3>
+<h3 style="text-align: center;"><a href="#">let\'s talk</a></h3>
+[/su_column]
+[/su_row]' ); ?>
 
 <?php get_footer(); ?>
